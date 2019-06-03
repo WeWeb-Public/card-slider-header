4f2d27d46b60e8da57b48b645e096eef25189a67
chihabeeddine <chihabeeddine.daoud@epitech.eu>
2019-06-03 14:10:58 +0200

    tablet layout

--- src/section.vue
+++ src/section.vue
@@ -382,34 +382,8 @@ export default {
 
         async openOptions() {
             try {
-                let editList = {
-                    WWSLIDER_CONFIG: {
-                        title: {
-                            en: 'Edit section',
-                            fr: 'Editer la section'
-                        },
-                        desc: {
-                            en: 'Edit section per line cards and navigation dots color',
-                            fr: 'Editer le nombre de carte par ligne et la couleur des points de navigation'
-                        },
-                        icon: 'wwi wwi-config',
-                        shortcut: 'g',
-                        next: 'WWSLIDER_CUSTOM'
-                    },
 
-                }
 
-                wwLib.wwPopups.addStory('WWSLIDER_CONFIG', {
-                    title: {
-                        en: 'Configuration',
-                        fr: 'Configurer'
-                    },
-                    type: 'wwPopupEditWwObject',
-                    buttons: null,
-                    storyData: {
-                        list: editList
-                    }
-                })
                 wwLib.wwPopups.addStory('WWSLIDER_CUSTOM', {
                     title: {
                         en: 'Fill in code',
@@ -459,7 +433,7 @@ export default {
                     }
                 })
                 let options = {
-                    firstPage: 'WWSLIDER_CONFIG',
+                    firstPage: 'WWSLIDER_CUSTOM',
                     data: {
                         columnPerLine: this.section.data.thumbnailsPerLine,
                         section: this.section,
@@ -528,16 +502,14 @@ export default {
     @media (min-width: 768px) {
         width: 80%;
     }
-    @media (min-width: 992px) {
-        width: 80%;
-    }
+
     @media (min-width: 1200px) {
         width: 70%;
     }
     .container-center {
         display: flex;
         transition: transform 0.5s ease;
-        @media (min-width: 768px) {
+        @media (min-width: 1024px) {
             justify-content: center;
             flex-wrap: wrap;
         }
@@ -634,14 +606,14 @@ export default {
 }
 .hidden-mobile {
     display: none;
-    @media (min-width: 768px) {
+    @media (min-width: 1024px) {
         display: block;
     }
 }
 .mobile-wrapper {
     display: block;
     position: relative;
-    @media (min-width: 768px) {
+    @media (min-width: 1024px) {
         display: none;
     }
 }
